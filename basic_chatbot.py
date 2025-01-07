965ad513e689246d046efeb1d347d09fc16edde5
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-07 19:30:21 +0530

    setup: Ensure NLTK corpora are available

--- basic_chatbot.py
+++ basic_chatbot.py
@@ -1,3 +1,13 @@
 import nltk
 import random
 from nltk.chat.util import Chat, reflections
+
+# Download NLTK resources if you haven't already
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+try:
+    nltk.data.find('corpora/omw-1.4')
+except LookupError:
+    nltk.download('omw-1.4')
