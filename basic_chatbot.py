efa77acdc9e954d36410668adb580a84e3c4af97
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-07 19:32:18 +0530

    feat: Implement chatbot conversation patterns using 'pairs' list

--- basic_chatbot.py
+++ basic_chatbot.py
@@ -11,3 +11,31 @@ try:
     nltk.data.find('corpora/omw-1.4')
 except LookupError:
     nltk.download('omw-1.4')
+
+# Define patterns and responses
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, how can I help you today?",]
+    ],
+    [
+        r"what is your name ?",
+        ["My name is Jarvis.",]
+    ],
+        [
+        r"who created you ?",
+        ["I was created by Keshav T M.",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing well, thank you!", "I'm fine, how about you?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["It's alright","No problem",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","Alright :)",]
+    ]
+]
